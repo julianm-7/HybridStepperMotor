2fe4519b4fe7b02214b7a7e21f0c7a7918691eef
Julian Montoya <146122211+julianm-7@users.noreply.github.com>
2025-09-08 13:07:21 -0400

    Create Crystalfontz128x128_ST7735.h

---
+++ include/LcdDrivermsp430/Crystalfontz128x128_ST7735.h
@@ -0,0 +1,7 @@
+#ifndef CRYSTALFONTZ128X128_ST7735_H
+#define CRYSTALFONTZ128X128_ST7735_H
+
+void Crystalfontz128x128_Init(void);
+void Crystalfontz128x128_SetOrientation(uint8_t orientation);
+
+#endif
